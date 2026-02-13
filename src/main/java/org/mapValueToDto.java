3b80093462af20e64828e955b78743cc9cd62b05
kedar000 <140879080+kedar000@users.noreply.github.com>
2026-02-13 11:13:07 +0530

    Added : node_modules

--- src/main/java/org/mapValueToDto.java
+++ src/main/java/org/mapValueToDto.java
@@ -1,4 +1,35 @@
 package org;
 
-public class mapValueToDto {
+
+private void mapValueToDto(ParticipantRowDto dto, String columnName, String value) {
+
+    switch (columnName) {
+        case "seeker name":
+            dto.setSeekerName(value);
+            break;
+
+        case "seeker email":
+            dto.setSeekerEmail(value);
+            break;
+
+        case "seeker phone no.":
+            dto.setSeekerPhone(value);
+            break;
+
+        case "provider name":
+            dto.setProviderName(value);
+            break;
+
+        case "provider email":
+            dto.setProviderEmail(value);
+            break;
+
+        case "relationship with seeker":
+            dto.setRelationshipWithSeeker(value);
+            break;
+
+        case "is family related":
+            dto.setFamilyRelated(Boolean.parseBoolean(value.toLowerCase()));
+            break;
+    }
 }
